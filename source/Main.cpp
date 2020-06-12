791ad4acfe5af0429f81a2205be411b631a286eb
Warsen <elias.p.carrillo@gmail.com>
2020-06-11 20:36:17 -0700

    Added single controller support for first recognized device

--- source/Main.cpp
+++ source/Main.cpp
@@ -15,6 +15,7 @@ static SDL_Window* window;
 static SDL_Renderer* renderer;
 static SDL_Texture* texture;
 static SDL_Texture* scanlineTexture;
+static SDL_GameController* gameController;
 static SMBEngine* smbEngine = nullptr;
 static uint32_t renderBuffer[RENDER_WIDTH * RENDER_HEIGHT];
 
@@ -71,7 +72,7 @@ static bool initialize()
     }
 
     // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) < 0)
     {
         std::cout << "SDL_Init() failed during initialize(): " << SDL_GetError() << std::endl;
         return false;
@@ -153,6 +154,9 @@ static bool initialize()
  */
 static void shutdown()
 {
+    if (gameController)
+        SDL_GameControllerClose(gameController);
+
     SDL_CloseAudio();
 
     SDL_DestroyTexture(scanlineTexture);
@@ -191,6 +195,21 @@ static void mainLoop()
                 }
                 break;
 
+            case SDL_CONTROLLERDEVICEADDED:
+                if (!gameController && event.cdevice.which == 0)
+                {
+                    gameController = SDL_GameControllerOpen(0);
+                }
+                break;
+
+            case SDL_CONTROLLERDEVICEREMOVED:
+                if (gameController && event.cdevice.which == 0)
+                {
+                    SDL_GameControllerClose(gameController);
+                    gameController = 0;
+                }
+                break;
+
             default:
                 break;
             }
@@ -198,14 +217,22 @@ static void mainLoop()
 
         const Uint8* keys = SDL_GetKeyboardState(NULL);
         Controller& controller1 = engine.getController1();
-        controller1.setButtonState(BUTTON_A, keys[SDL_SCANCODE_X]);
-        controller1.setButtonState(BUTTON_B, keys[SDL_SCANCODE_Z]);
-        controller1.setButtonState(BUTTON_SELECT, keys[SDL_SCANCODE_BACKSPACE]);
-        controller1.setButtonState(BUTTON_START, keys[SDL_SCANCODE_RETURN]);
-        controller1.setButtonState(BUTTON_UP, keys[SDL_SCANCODE_UP]);
-        controller1.setButtonState(BUTTON_DOWN, keys[SDL_SCANCODE_DOWN]);
-        controller1.setButtonState(BUTTON_LEFT, keys[SDL_SCANCODE_LEFT]);
-        controller1.setButtonState(BUTTON_RIGHT, keys[SDL_SCANCODE_RIGHT]);
+        controller1.setButtonState(BUTTON_A, keys[SDL_SCANCODE_X]
+            || (gameController && SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_A)));
+        controller1.setButtonState(BUTTON_B, keys[SDL_SCANCODE_Z]
+            || (gameController && SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_X)));
+        controller1.setButtonState(BUTTON_SELECT, keys[SDL_SCANCODE_BACKSPACE]
+            || (gameController && SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_BACK)));
+        controller1.setButtonState(BUTTON_START, keys[SDL_SCANCODE_RETURN]
+            || (gameController && SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_START)));
+        controller1.setButtonState(BUTTON_UP, keys[SDL_SCANCODE_UP]
+            || (gameController && SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_DPAD_UP)));
+        controller1.setButtonState(BUTTON_DOWN, keys[SDL_SCANCODE_DOWN]
+            || (gameController && SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_DPAD_DOWN)));
+        controller1.setButtonState(BUTTON_LEFT, keys[SDL_SCANCODE_LEFT]
+            || (gameController && SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_DPAD_LEFT)));
+        controller1.setButtonState(BUTTON_RIGHT, keys[SDL_SCANCODE_RIGHT]
+            || (gameController && SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_DPAD_RIGHT)));
 
         if (keys[SDL_SCANCODE_R])
         {
