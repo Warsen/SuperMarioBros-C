b02a051f258e54abacadadaf1abf7a3e95492650
Warsen <elias.p.carrillo@gmail.com>
2023-04-25 02:19:46 -0700

    Documentation for macros.

--- source/SMB/SMB.hpp
+++ source/SMB/SMB.hpp
@@ -8,29 +8,34 @@
 // Macros:
 //---------------------------------------------------------------------
 
-/**
- * Read and access a byte from emulated memory.
- */
+/// <summary>
+/// Returns a MemoryAccess object for the specified memory address.
+/// </summary>
+/// <param name="address">The 16-bit memory address.</param>
+/// <returns>A MemoryAccess object for the specified memory address.</returns>
 #define M(addr) getMemory(addr)
 
-/**
- * Read a word from emulated memory (in little-endian format).
- */
+/// <summary>
+/// Get a word of memory from a zero-page address and the next byte (wrapped around) in little-endian format.
+/// Returns a 16-bit value from memory starting at the specified address. Little-endian byte order.
+/// </summary>
+/// <param name="address">The 8-bit memory address of the least significant byte.</param>
+/// <returns>The 16-bit value from memory starting at the specified address.</returns>
 #define W(addr) getMemoryWord(addr)
 
-/**
- * Call a subroutine stored in a goto label.
- */
+/// <summary>
+/// Call a subroutine stored in a goto label. Pushes a return index to a generated return label.
+/// </summary>
 #define JSR(subroutine, index) pushReturnIndex(index); goto subroutine; Return_ ## index:
 
-/**
- * High/upper byte of a 16-bit integer.
- */
+/// <summary>
+/// Gets the high byte of a 16-bit integer.
+/// </summary>
 #define HIBYTE(v) (static_cast<uint8_t>((v >> 8) & 0xff))
 
-/**
- * Low byte of a 16-bit integer.
- */
+/// <summary>
+/// Gets the low byte of a 16-bit integer.
+/// </summary>
 #define LOBYTE(v) (static_cast<uint8_t>(v & 0xff))
 
 #endif // SMB_HPP
